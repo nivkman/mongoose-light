12e26eaed5f9036bb3580f7fc0a13ffbe49dd3a5
Niv Kaufman <nivkman@gmail.com>
2024-09-19 14:34:00 +0300

    add try to connect mongoose

--- src/initializeDb.js
+++ src/initializeDb.js
@@ -3,21 +3,22 @@ import mongoose from "mongoose";
 mongoose.Promise = global.Promise;
 
 class InitializeDb {
-  async initializeMongoDb({databaseUrl, service}) {
-    mongoose.connect(
-      `${databaseUrl}/${service}`,
-      { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
-      (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("[mongodb] DB connected successfully");
-        }
-      }
-    );
+  async initializeMongoDb({ databaseUrl, service = "" }) {
+    try {
+      const servicePath = service ? `/${service}` : "";
+      mongoose.connect(`${databaseUrl}${servicePath}`, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      });
+      console.log("[mongodb] DB connected successfully");
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
 const initializeDb = new InitializeDb();
 
-export const initializeMongoDb = (config) => initializeDb.initializeMongoDb(config);
+export const initializeMongoDb = ({ databaseUrl, service }) =>
+  initializeDb.initializeMongoDb({ databaseUrl, service });
